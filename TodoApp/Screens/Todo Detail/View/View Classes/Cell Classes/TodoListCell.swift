a6de9aa2818c8aa497082ab46190d15c74080df5
Mehmet Anıl Kul <mehmetanilkul@gmail.com>
2020-12-20 17:02:19 +0300

    Add todo list cell class and ui

---
+++ TodoApp/Screens/Todo Detail/View/View Classes/Cell Classes/TodoListCell.swift
@@ -0,0 +1,20 @@
+//
+//  TodoListCell.swift
+//  TodoApp
+//
+//  Created by Mehmet Anıl Kul on 20.12.2020.
+//
+
+import UIKit
+
+class TodoListCell: UITableViewCell {
+  @IBOutlet weak var titleLabel: UILabel!
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+  
+  @IBAction func todoItemPressed(_ sender: UIButton) {
+    
+  }
+}
---
+++ TodoApp/Screens/Todo List/View Model/Main View Models/TodoListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TodoListViewModel.swift
+//  TodoApp
+//
+//  Created by Mehmet Anıl Kul on 20.12.2020.
+//
+
+import Foundation
