918f532554e9d17c3bde98f2cc42326c627a414d
Mehmet Anıl Kul <mehmetanilkul@gmail.com>
2020-12-20 13:53:53 +0300

    Initial Commit

---
+++ TodoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoApp
+//
+//  Created by Mehmet Anıl Kul on 20.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
