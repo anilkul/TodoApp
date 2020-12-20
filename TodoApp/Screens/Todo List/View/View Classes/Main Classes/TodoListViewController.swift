7eba8af98466a131ae77058ab608811a830d8ae2
Mehmet Anıl Kul <mehmetanilkul@gmail.com>
2020-12-20 14:33:49 +0300

    Connect outlets and add bar button items

---
+++ TodoApp/Screens/Todo List/View/View Classes/Main Classes/TodoListViewController.swift
@@ -0,0 +1,23 @@
+//
+//  TodoListViewController.swift
+//  TodoApp
+//
+//  Created by Mehmet Anıl Kul on 20.12.2020.
+//
+
+import UIKit
+
+class TodoListViewController: UIViewController {
+  @IBOutlet weak var tableView: UITableView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+  
+  
+  @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    
+  }
+}
+
